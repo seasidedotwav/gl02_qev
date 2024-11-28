baf11e68899d6b3846c77b8da6e74c16bacea326
Edgar <edgar.clerc@yahoo.com>
2024-11-28 19:12:59 +0100

    question object and start parser

---
+++ QUESTION.js
@@ -0,0 +1,22 @@
+/*   exemple question :
+                id                                        question
+::U3 p31 6 -ed adjectives and prepositions:: Choose the correct preposition.
+    text                            answer                       text
+What sports do you get excited {1:MC:~with~=about}? What sports do you find exciting?
+Do you ever get frustrated {1:MC:~on~=with} a sportsperson/team? Which person/team do you find frustrating?
+Who are you impressed {1:MC:~=by~for}? Who do you find impressive?
+Who or what do you get annoyed {1:MC:~of~=by}?
+What do you sometimes feel embarrassed {1:MC:~in~=by}? Who do you find embarrassing?
+*/
+
+
+var QUESTION = function(id,questions, answer, text){
+    this.id = id
+	this.questions = questions;
+    this.text = text
+	this.answer = answer;
+}
+
+
+
+module.exports = QUESTION;
